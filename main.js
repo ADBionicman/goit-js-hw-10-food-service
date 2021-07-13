fdaefbb40860ed050b24a684fd90f6f10d85680c
ADBionicman <36104341+ADBionicman@users.noreply.github.com>
2021-07-13 20:13:20 +0300

    1.2

---
+++ main.js
@@ -0,0 +1,12 @@
+const clients = ['Mango', 'Ajax', 'Poly'];
+
+for (const client of clients) {
+  console.log(client);
+}
+
+// Итерация по строке
+const string = 'javascript';
+
+for (const character of string) {
+  console.log(character);
+}
